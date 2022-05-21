43444256e72c98720b5767226a72d266ad89cd68
Mark Fox <markfox47@hotmail.com>
2022-05-21 16:01:33 -0500

    worked on linking and redirecting pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 console.log("js page reached");
+
+// function startup() {
+//     const loginButton = document.getElementById("loginBtn");
+//     loginButton.onclick = login();
+// }
+
+function login () {
+    console.log("login function reached...");
+    let username = document.getElementById("usernameLogin").value;
+    window.location.href="/pages/dashboard.html";
+    console.log(username);
+}
+// startup();
