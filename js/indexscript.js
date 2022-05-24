9234076c3ae02ec4c435a54e6eb81feb57313584
Mark Fox <markfox47@hotmail.com>
2022-05-24 11:37:39 -0500

    added validation for login info

--- js/indexscript.js
+++ js/indexscript.js
@@ -1,4 +1,5 @@
 console.log("js page reached");
+const emailCheck = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}', 'g');
 
 function startup() {
     console.log("startup reached...");
@@ -7,12 +8,21 @@ function startup() {
 
 function login (event) {
     event.preventDefault();
+    const name = "a@email.com";
+    const pass = "b";
     console.log("login function reached...");
     let username = document.getElementById("usernameLogin").value;
     let password = document.getElementById("passwordLogin").value;
-    // window.location.href="./pages/dashboard.html";
-    console.log(username);
-    console.log(password);
+
+    // console.log(emailCheck.test(username));
+
+    if (username == name && password == pass && emailCheck.test(username)) {
+        window.location.href="./pages/dashboard.html";
+    } else {
+        document.getElementById("invalidLogin").innerHTML = "Invalid Username/Password";
+    }    
+    // console.log(username);
+    // console.log(password);
 }
 
 
