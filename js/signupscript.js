b62af5151252fb1e57903470ffb26becdf84aa52
Mark Fox <markfox47@hotmail.com>
2022-05-24 19:00:53 -0500

    added validation to signup page

--- js/signupscript.js
+++ js/signupscript.js
@@ -1,4 +1,4 @@
-
+const emailCheck = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}', 'g');
 
 function startSignup() {
     document.getElementById("signupForm").addEventListener("submit", signup);
@@ -8,7 +8,22 @@ function startSignup() {
 function signup(event) {
     event.preventDefault();
     console.log("form submitted...");
-    window.location.href = "./dashboard.html";
+    let usernameSignup = document.getElementById("usernameSignup").value;
+    let invalid = document.getElementById("invalidSignup");
+// TODO change back after testing
+    if (!emailCheck.test(usernameSignup)) {
+        let pass1 = document.getElementById("passwordSignup").value;
+        let pass2 = document.getElementById("passwordSignup2").value;
+        // console.log(typeof pass1);
+        // console.log(typeof pass2);
+        if (pass1 != "" && pass2 != "" && pass1 == pass2) {
+            window.location.href = "./dashboard.html";
+        } else {
+            invalid.innerHTML = "Both password fields must match";
+        }                
+    } else {
+        invalid.innerHTML = "Please enter a valid email address";
+    }
     // alert("new account created");
 }
 
